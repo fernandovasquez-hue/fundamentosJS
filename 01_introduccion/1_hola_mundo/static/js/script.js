745babe7309df65bef75c716d2b230b90c9dd65d
fernandovasquez-hue <fernandovasquez@liceovvh.cl>
2026-04-09 12:58:31 -0400

    ok

---
+++ 01_introduccion/1_hola_mundo/static/js/script.js
@@ -0,0 +1,13 @@
+// prueba de conexion con js
+console.log("Hola mundo, desde archivo externo!...");
+
+// 1) hola mund con variable y const.
+const saludo = "Hola a todos, ";
+let destinatario = "estudiantes";
+// impresion de saludo concatenando con (+)
+console.log(saludo + "para los " + destinatario);
+
+// 2)saludar a una persona ingresada por teclado
+let nombre = prompt("ingrese su nombre: ");
+//imprimir un nombre ingresado 
+console.log("hola, " + nombre);
---
+++ 02_tipos_de_datos/1_Tipo_texto/static/js/script.js
@@ -0,0 +1,10 @@
+console.log("conexion con js correcta...");
+/*
+Dato: Tipo de texto(string)
+Concatenacion de texto con el signo +
+Podemos unir texto y variables
+*/
+const nombre = "Fernando"
+const apellido = "Vásquez"
+//unimos ambas constantes con un texto extra
+console.log("Hola, mi nombre es: " + nombre + " " + apellido);
