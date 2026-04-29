01d990319c26bc62ad8e616495b4c09cb2f4c937
fernandovasquez-hue <fernandovasquez@liceovvh.cl>
2026-04-29 16:08:52 -0400

    ok

---
+++ 03_estructura_de_control/2.-Practicando_condiciones/static/js/scrpt.js
@@ -0,0 +1,88 @@
+// EJERCICIO 1
+// Resultado esperado en alert:
+// "Bienvenido Juan"
+
+function ejercicio1() {
+
+}
+
+
+// EJERCICIO 2
+// Resultado esperado en alert:
+// "La suma es: 15"
+
+function ejercicio2() {
+
+}
+
+
+// EJERCICIO 3
+// Resultado esperado en alert:
+// "La resta es: 12"
+
+function ejercicio3() {
+
+}
+
+
+// EJERCICIO 4
+// Resultado esperado en alert:
+// "La multiplicación es: 24"
+
+function ejercicio4() {
+
+}
+
+
+// EJERCICIO 5
+// Resultado esperado en alert:
+// "El promedio es: 5.7"
+
+function ejercicio5() {
+
+}
+
+
+// EJERCICIO 6
+// Resultado esperado en alert:
+// "Es mayor de edad"
+
+function ejercicio6() {
+
+}
+
+
+// EJERCICIO 7
+// Resultado esperado en alert:
+// "El número es par"
+
+function ejercicio7() {
+
+}
+
+
+// EJERCICIO 8
+// Resultado esperado en alert:
+// "Estudiante aprobado"
+
+function ejercicio8() {
+
+}
+
+
+// EJERCICIO 9
+// Resultado esperado en alert:
+// "Precio final con descuento: $18000"
+
+function ejercicio9() {
+
+}
+
+
+// EJERCICIO 10
+// Resultado esperado en alert:
+// "El número mayor es: 22"
+
+function ejercicio10() {
+
+}
